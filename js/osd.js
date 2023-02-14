ac98860ec6b11d1af479b70fab7c801a01dc7ace
linxOD <linxOD@users.noreply.github.com>
2023-02-14 13:52:47 +0000

    deploy: a199bcb96aa4fc4d1a6e647643fdee2dc1211548

--- js/osd.js
+++ js/osd.js
@@ -12,7 +12,7 @@ if (container !== null) {
     var width = container.clientWidth;
 }
 var container = document.getElementById("viewer-1");
-container.style.width = `${String(width)}px`;
+container.style.width = `${String(width - 50)}px`;
 
 var tileSources = [];
 var img = document.getElementsByClassName("tei-xml-images");
@@ -27,9 +27,66 @@ var viewer = OpenSeadragon({
     prefixUrl: 'https://cdnjs.cloudflare.com/ajax/libs/openseadragon/2.4.1/images/',
     sequenceMode: true,
     showNavigator: true,
-    imageLoaderLimit: 10,
+    imageLoaderLimit: 20,
     tileSources: tileSources
 });
 setTimeout(function() {
     document.getElementById("container_facs_2").remove();
 }, 1000);
+
+// Get the an HTML element
+var element = document.getElementsByClassName('pb');
+var idx = 0;
+var prev_idx = -1;
+
+var position = document.documentElement.scrollTop;
+
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function(event) {
+    var scroll = document.documentElement.scrollTop;
+    // console.log("Position");
+    // console.log(position);
+    // console.log("Scroll");
+    // console.log(scroll);
+    // console.log("El offsettop");
+    // console.log(element[idx].offsetTop);
+    if (scroll > position) {
+        if (isInViewport(element[idx])) {
+            viewer.goToNextPage();
+            idx += 1;
+            prev_idx += 1;
+        }
+    } else {
+        if (prev_idx >= 0 && isInViewport(element[prev_idx])) {
+            viewer.goToPreviousPage();
+            idx -= 1;
+            prev_idx -= 1;
+        } 
+    }
+    position = document.documentElement.scrollTop;
+}, false);
+
+function isInViewport(element) {
+    // Get the bounding client rectangle position in the viewport
+    var bounding = element.getBoundingClientRect();
+    // Checking part. Here the code checks if it's *fully* visible
+    // Edit this part if you just want a partial visibility
+    if (
+        bounding.top <= 180 &&
+        bounding.bottom <= 210 &&
+        bounding.top >= 0 &&
+        bounding.bottom >= 0
+        // bounding.top >= 0 &&
+        // bounding.left >= 0 &&
+        // bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+        // bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    ) {
+        return true;
+    } else {
+        // console.log("Top");
+        // console.log(bounding.top);
+        // console.log("Bottom");
+        // console.log(bounding.bottom);
+        return false;
+    }
+}
