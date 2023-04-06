81a1f1511ed7470a4b377eac90c59ddc485f20e2
linxOD <linxOD@users.noreply.github.com>
2023-04-06 12:22:47 +0000

    deploy: b004d8411d5b9fc01eb178c9445bcfd5180296cf

--- js/update_doc_language.js
+++ js/update_doc_language.js
@@ -1,43 +1,103 @@
 /*
-    function to update document language
+    mapping to update document language
     based on url search parameters
 */
-(function updateDocLanguage() {
-    var url = new URL(document.location.href);
-    var urlParam = new URLSearchParams(url.search);
+(async function getMappingJson() {
+    // let file = "https://raw.githubusercontent.com/Hanslick-Online/hsl-app/main/html/json";
+    let file = "http://0.0.0.0:8000/json";
+    const response = await fetch(`${file}/lang-mapping.json`);
+    const jsonData = await response.json();
 
-    if (urlParam.get("lang") == "de") {
-        document.getElementsByClassName("navbar-nav-de")[0]
-        .classList.remove("fade");
-        document.getElementsByClassName("navbar-nav-en")[0]
-        .classList.add("fade");
-    } else {
-        document.getElementsByClassName("navbar-nav-en")[0]
-        .classList.remove("fade");
-        document.getElementsByClassName("navbar-nav-de")[0]
-        .classList.add("fade");
+    window.onload = loadLanguage();
+    
+    function loadLanguage() {
+        var url = new URL(document.location.href);
+        var urlParam = new URLSearchParams(url.search);
+        var lang = urlParam.get("lang");
+
+        let path = location.pathname.replace("/", "");
+        let newPath = jsonData[lang][path];
+
+        if (newPath) {
+            window.history.replaceState({}, "", `${newPath}?${urlParam}`);
+            location.reload();
+            return false;
+        } else {
+            window.history.replaceState({}, "", `${location.pathname}?${urlParam}`);
+        }
+        
     }
 
-    var lang_btn_de = document.getElementsByClassName("lang_change_de");
-    var lang_btn_en = document.getElementsByClassName("lang_change_en");
-    [].forEach.call(lang_btn_de, function(opt) {
-        opt.addEventListener("click", function(event) {
-            event.preventDefault();
-            urlParam.set("lang", "de");
-            /* replace state to change url with new urlparam */
-            window.history.replaceState({}, "", `${location.pathname}?${urlParam}`);
-            location.reload();
-            return false;
+    /*
+        function to update document language
+        based on url search parameters
+    */
+    (function updateDocLanguage() {
+        var url = new URL(document.location.href);
+        var urlParam = new URLSearchParams(url.search);
+
+        var translate_de = document.getElementsByClassName("translate-de");
+        var translate_en = document.getElementsByClassName("translate-en");
+
+        if (urlParam.get("lang") == "de") {
+            [].forEach.call(translate_de, function(opt) {
+                opt.classList.remove("fade");
+            });
+            [].forEach.call(translate_en, function(opt) {
+                opt.classList.add("fade");
+            });
+        } else {
+            [].forEach.call(translate_en, function(opt) {
+                opt.classList.remove("fade");
+            });
+            [].forEach.call(translate_de, function(opt) {
+                opt.classList.add("fade");
+            });
+        }
+
+        var lang_btn_de = document.getElementsByClassName("lang_change_de");
+        var lang_btn_en = document.getElementsByClassName("lang_change_en");
+        [].forEach.call(lang_btn_de, function(opt) {            
+            opt.addEventListener("click", function(event) {
+                event.preventDefault();
+
+                var loc = location.pathname.replace("/", "");
+                var path = jsonData.de[loc];
+                urlParam.set("lang", "de");
+
+                if (path) {
+                    /* replace state to change url with new urlparam */
+                    window.history.replaceState({}, "", `${path}?${urlParam}`);
+                    location.reload();
+                    return false;
+                } else {
+                    /* replace state to change url with new urlparam */
+                    window.history.replaceState({}, "", `${location.pathname}?${urlParam}`);
+                    location.reload();
+                    return false;
+                }
+            });
         });
-    });
-    [].forEach.call(lang_btn_en, function(opt) {
-        opt.addEventListener("click", function(event) {
-            event.preventDefault();
-            urlParam.set("lang", "en");
-            /* replace state to change url with new urlparam */
-            window.history.replaceState({}, "", `${location.pathname}?${urlParam}`);
-            location.reload();
-            return false;
+        [].forEach.call(lang_btn_en, function(opt) {
+            opt.addEventListener("click", function(event) {
+                event.preventDefault();
+
+                var loc = location.pathname.replace("/", "");
+                var path = jsonData.en[loc];
+                urlParam.set("lang", "en");
+
+                if (path) {
+                    /* replace state to change url with new urlparam */
+                    window.history.replaceState({}, "", `${path}?${urlParam}`);
+                    location.reload();
+                    return false;
+                } else {
+                    /* replace state to change url with new urlparam */
+                    window.history.replaceState({}, "", `${location.pathname}?${urlParam}`);
+                    location.reload();
+                    return false;
+                }
+            });
         });
-    });
+    })();
 })();
