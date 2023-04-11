e84200de8b2dd578781febec9d4be6f487c9b345
linxOD <linxOD@users.noreply.github.com>
2023-04-11 14:03:40 +0000

    deploy: 792b36a5f0c07bccc67fbf9bfb6c9f3677261285

--- js/hide-md.js
+++ js/hide-md.js
@@ -1,6 +1,6 @@
 $('#show-text').click(function () {
-    if ($('.about-text-hidden').hasClass('fade') == true) {
-        $('.about-text-hidden').removeClass('fade')
+    if ($('.about-text-hidden').hasClass('fade-lang') == true) {
+        $('.about-text-hidden').removeClass('fade-lang')
         .addClass('active');
         if ($(this).text() == "mehr anzeigen") {
             $(this).html("weniger anzeigen");
@@ -10,7 +10,7 @@ $('#show-text').click(function () {
         
     } else {
         $('.about-text-hidden').removeClass('active')
-        .addClass('fade');
+        .addClass('fade-lang');
         if ($(this).text() == "weniger anzeigen") {
             $(this).html("mehr anzeigen");
         } else {
