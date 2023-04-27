2321a6872336a38c7faa97483688fa9d563ace7c
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-04-27 08:12:43 +0200

    enable denorm... indices for traktat

--- build_app/python/denormalize_indices.py
+++ build_app/python/denormalize_indices.py
@@ -1,16 +1,16 @@
 from utils import denormalize_indices
 
 
-# files = "data/traktat/editions/*.xml"
-# indices = "data/indices/*.xml"
-# mention_xpath = ".//tei:rs[@ref]/@ref"
-# event_title = "erwähnt in "
-# title_xpath = ".//tei:title[@type='main']/text()"
-# title_xpath_sub = ".//tei:sourceDesc//tei:edition/text()"
+files = "data/traktat/editions/*.xml"
+indices = "data/indices/*.xml"
+mention_xpath = ".//tei:rs[@ref]/@ref"
+event_title = "erwähnt in "
+title_xpath = ".//tei:title[@type='main']/text()"
+title_xpath_sub = ".//tei:sourceDesc//tei:edition/text()"
 
-# denormalize_indices(
-#     files, indices, mention_xpath, event_title, title_xpath, title_xpath_sub, blacklist_ids=[]
-# )
+denormalize_indices(
+    files, indices, mention_xpath, event_title, title_xpath, title_xpath_sub, blacklist_ids=[]
+)
 
 files = "data/critics/editions/*.xml"
 indices = "data/indices/*.xml"
