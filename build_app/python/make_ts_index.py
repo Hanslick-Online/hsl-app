8f6a1454020d606fab507b1661c5af05c2d9cbf5
Daniel_Stoxreiter <elsner.daniel@outlook.com>
2023-08-31 10:24:10 +0200

    pot. fix to remove trailing whitespaces in search result #5

--- build_app/python/make_ts_index.py
+++ build_app/python/make_ts_index.py
@@ -72,12 +72,15 @@ current_schema = {
 
 client.collections.create(current_schema)
 
+
 def get_entities(ent_type, ent_node, ent_name):
     entities = []
     e_path = f'.//tei:rs[@type="{ent_type}"]/@ref'
     for p in body:
-        ent = p.xpath(e_path, namespaces={'tei': "http://www.tei-c.org/ns/1.0"})
-        ref = [ref.replace("#", "") for e in ent if len(ent) > 0  for ref in e.split()]
+        ent = p.xpath(e_path,
+                      namespaces={'tei': "http://www.tei-c.org/ns/1.0"})
+        ref = [ref.replace("#", "")
+               for e in ent if len(ent) > 0 for ref in e.split()]
         for r in ref:
             p_path = f'.//tei:{ent_node}[@xml:id="{r}"]//tei:{ent_name}[1]'
             en = doc.any_xpath(p_path)
@@ -89,6 +92,7 @@ def get_entities(ent_type, ent_node, ent_name):
                     with open("log-entities.txt", "a") as f:
                         f.write(f"{r} in {record['id']}\n")
     return [ent for ent in sorted(set(entities))]
+
 
 records = []
 cfts_records = []
@@ -106,12 +110,19 @@ for x in tqdm(files, total=len(files)):
                 'project': 'hsl',
             }
             record = {}
-            record['id'] = os.path.split(x)[-1].replace('.xml', f".html#index.xml-body.1_div.{str(pages)}")
+            anchor_id_1 = os.path.split(x)[-1]
+            anchor_id_2 = ".html#index.xml-body.1_div."
+            record['id'] = anchor_id_1.replace('.xml',
+                                               f"{anchor_id_2}{str(pages)}")
             cfts_record['id'] = record['id']
-            cfts_record['resolver'] = f"https://hanslick.acdh.oeaw.ac.at/{record['id']}"
+            hsl_url = "https://hanslick.acdh.oeaw.ac.at"
+            cfts_record['resolver'] = f"{hsl_url}/{record['id']}"
             record['rec_id'] = os.path.split(x)[-1]
             cfts_record['rec_id'] = record['rec_id']
-            r_title = " ".join(" ".join(doc.any_xpath('.//tei:titleStmt/tei:title[@type="main"]/text()')).split())
+            r_title = " ".join(
+                " ".join(doc.any_xpath(
+                        './/tei:titleStmt/tei:title[@type="main"]/text()')
+                        ).split())
             s_title = doc.any_xpath('.//tei:sourceDesc//tei:edition/@n')[0]
             title = f"{r_title} {s_title}. Auflage"
             if pages - 1 == 0:
@@ -123,10 +134,14 @@ for x in tqdm(files, total=len(files)):
             try:
                 date_str = doc.any_xpath('//tei:sourceDesc//tei:date/@when')[0]
                 date_seq = f"{date_str}-0{str(pages)}-01"
-                date_seq = time.mktime(datetime.datetime.strptime(date_seq, "%Y-%m-%d").timetuple())
+                date_seq = time.mktime(datetime.datetime.strptime(date_seq,
+                                                                  "%Y-%m-%d"
+                                                                  ).timetuple()
+                                       )
             except IndexError:
                 try:
-                    date_str = doc.any_xpath('//tei:sourceDesc//tei:date/text()')[0]
+                    date_str = doc.any_xpath(
+                        '//tei:sourceDesc//tei:date/text()')[0]
                 except IndexError:
                     date_str = "0"
             if len(date_str) > 3:
@@ -146,19 +161,26 @@ for x in tqdm(files, total=len(files)):
                 # get unique persons per page
                 ent_type = "person"
                 ent_name = "persName"
-                record['persons'] = get_entities(ent_type=ent_type, ent_node=ent_type, ent_name=ent_name)
+                record['persons'] = get_entities(ent_type=ent_type,
+                                                 ent_node=ent_type,
+                                                 ent_name=ent_name)
                 cfts_record['persons'] = record['persons']
                 # get unique places per page
                 ent_type = "place"
                 ent_name = "placeName"
-                record['places'] = get_entities(ent_type=ent_type, ent_node=ent_type, ent_name=ent_name)
+                record['places'] = get_entities(ent_type=ent_type,
+                                                ent_node=ent_type,
+                                                ent_name=ent_name)
                 cfts_record['places'] = record['places']
                 # get unique bibls per page
                 ent_type = "bibl"
                 ent_name = "title"
-                record['works'] = get_entities(ent_type=ent_type, ent_node=ent_type, ent_name=ent_name)
+                record['works'] = get_entities(ent_type=ent_type,
+                                               ent_node=ent_type,
+                                               ent_name=ent_name)
                 cfts_record['works'] = record['works']
-                record['full_text'] = "\n".join(" ".join("".join(p.itertext()).split()) for p in body)
+                record['full_text'] = "\n".join(" ".join("".join(
+                    p.itertext()).split()) for p in body)
                 if len(record['full_text']) > 0:
                     records.append(record)
                     cfts_record['full_text'] = record['full_text']
@@ -168,19 +190,24 @@ for x in tqdm(files, total=len(files)):
         pages = 0
         for v in facs:
             pages += 1
-            p_group = f".//tei:body/tei:div[{pages}]/tei:p"
+            p_group = f".//tei:body/tei:div[{pages}]/tei:p/text()"
             body = doc.any_xpath(p_group)
             cfts_record = {
                 'project': 'hsl',
             }
             record = {}
-            record['id'] = os.path.split(x)[-1].replace('.xml', f".html")
+            record['id'] = os.path.split(x)[-1].replace('.xml', ".html")
             cfts_record['id'] = record['id']
-            cfts_record['resolver'] = f"https://hanslick-online.github.io/hsl-app-dev/{record['id']}"
+            hsl_url_dev = "https://hanslick-online.github.io/hsl-app-dev"
+            # hsl_url = "https://hanslick.acdh.oeaw.ac.at"
+            cfts_record['resolver'] = f"{hsl_url_dev}/{record['id']}"
             record['rec_id'] = os.path.split(x)[-1]
             cfts_record['rec_id'] = record['rec_id']
-            r_title = " ".join(" ".join(doc.any_xpath('.//tei:titleStmt/tei:title[@level="a"]/text()')).split())
-            s_title = doc.any_xpath('.//tei:titleStmt/tei:title[@level="s"]/text()')[0]
+            r_title = " ".join(" ".join(
+                doc.any_xpath('.//tei:titleStmt/tei:title[@level="a"]/text()')
+            ).split())
+            s_title = doc.any_xpath(
+                './/tei:titleStmt/tei:title[@level="s"]/text()')[0]
             title = f"{r_title} {s_title}"
             record['title'] = title
             cfts_record['title'] = record['title']
@@ -188,7 +215,9 @@ for x in tqdm(files, total=len(files)):
                 date_str = doc.any_xpath('//tei:sourceDesc//tei:date/@when')[0]
                 if len(date_str) == 4:
                     date_str = f"{date_str}-01-01"
-                date_seq = time.mktime(datetime.datetime.strptime(date_str, "%Y-%m-%d").timetuple())
+                date_seq = time.mktime(datetime.datetime.strptime(date_str,
+                                                                  "%Y-%m-%d")
+                                       .timetuple())
             except IndexError:
                 date_str = "0000-00-00"
                 date_seq = "0000-00-00"
@@ -204,19 +233,26 @@ for x in tqdm(files, total=len(files)):
                 # get unique persons per page
                 ent_type = "person"
                 ent_name = "persName"
-                record['persons'] = get_entities(ent_type=ent_type, ent_node=ent_type, ent_name=ent_name)
+                record['persons'] = get_entities(ent_type=ent_type,
+                                                 ent_node=ent_type,
+                                                 ent_name=ent_name)
                 cfts_record['persons'] = record['persons']
                 # get unique places per page
                 ent_type = "place"
                 ent_name = "placeName"
-                record['places'] = get_entities(ent_type=ent_type, ent_node=ent_type, ent_name=ent_name)
+                record['places'] = get_entities(ent_type=ent_type,
+                                                ent_node=ent_type,
+                                                ent_name=ent_name)
                 cfts_record['places'] = record['places']
                 # get unique bibls per page
                 ent_type = "bibl"
                 ent_name = "title"
-                record['works'] = get_entities(ent_type=ent_type, ent_node=ent_type, ent_name=ent_name)
+                record['works'] = get_entities(ent_type=ent_type,
+                                               ent_node=ent_type,
+                                               ent_name=ent_name)
                 cfts_record['works'] = record['works']
-                record['full_text'] = "\n".join(" ".join("".join(p.itertext()).split()) for p in body)
+                record['full_text'] = "\n".join(" ".join(
+                    "".join(p.itertext()).split()) for p in body)
                 if len(record['full_text']) > 0:
                     records.append(record)
                     cfts_record['full_text'] = record['full_text']
@@ -226,6 +262,7 @@ make_index = client.collections['hsl'].documents.import_(records)
 print(make_index)
 print('done with indexing hsl')
 
-make_index = CFTS_COLLECTION.documents.import_(cfts_records, {'action': 'upsert'})
+make_index = CFTS_COLLECTION.documents.import_(cfts_records,
+                                               {'action': 'upsert'})
 print(make_index)
 print('done with cfts-index hsl')
