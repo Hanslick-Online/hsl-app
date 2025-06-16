7925fa5f16c0cf3937a589d0d357a05d3d27f784
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2025-06-16 09:28:16 +0200

    removing debugging prints

--- build_app/python/xml2latex.py
+++ build_app/python/xml2latex.py
@@ -11,7 +11,6 @@ def fix_invalid_xml_id(xml_text):
     return re.sub(r'xml:id="([^a-zA-Z_])', r'xml:id="_\1', xml_text)
 
 def make_name_list(names):
-    print(names)
     last = True
     names = [
         " ".join([n.strip() for n in name.split(",")][::-1])
@@ -77,7 +76,6 @@ def get_info(tree):
             titles  += tree.xpath(".//tei:titleStmt/tei:title[@level='j']/text()", namespaces=ns)
     else:
         titles = tree.xpath(".//tei:analytic/tei:title/text()", namespaces=ns) + tree.xpath(".//tei:monogr/tei:title/text()", namespaces=ns)
-    print(titles)
     if tree.xpath(".//tei:titleStmt/tei:authors", namespaces=ns):
         authorsb =  tree.xpath(".//tei:titleStmt/tei:authors/text()", namespaces=ns)
     else:
@@ -149,12 +147,9 @@ def transform_tei_to_latex(input_file, output_file):
 
     # Example: Extracting some TEI elements and converting to LaTeX
     if Titles:
-        print(Titles)
         Title = Titles[0]
         if Titles[1:]:
-            print(Titles[1:])
             Subtitle = "\\\\".join([f"\\Large{{{title}}}" for title in Titles[1:]])
-            print(Subtitle)
             Title = f"{Title}\\\\{Subtitle}"
         if Editors:
             Title = f"{Title}\\\\\\large{{Herausgegeben von {Editors}}}"
