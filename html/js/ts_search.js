b0c454df291c0f8d6df1672592f8503e8695cca2
Fernando Sanz-Lázaro <54422069+fsanzl@users.noreply.github.com>
2025-08-11 10:08:43 +0200

    Update ts_search.js

--- html/js/ts_search.js
+++ html/js/ts_search.js
@@ -73,6 +73,10 @@ search.addWidgets([
               </div>
           `,
     },
+    return items.map(item => ({
+      ...item,
+      id: item.id.replace(/t__(\d\d)_VMS_(\d\d\d\d)_TEI_AW_(\d\d-\d\d-\d\d)-TEI-P5\.html/, 't__VMS_Auflage_$1_$2.html')
+    }));
   }),
 
   instantsearch.widgets.stats({
