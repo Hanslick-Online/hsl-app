5d45f136370eb2b4b88b41a147f5f8751e86d45f
fsanzl <>
2025-10-31 10:06:00 +0100

    names

--- build_app/shell/full_workflow.sh
+++ build_app/shell/full_workflow.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ANT_OPTS="-Xmx5g"
-TYPESENSE_HOST="typesense.acdh-dev.oeaw.ac.at"
-TYPESENSE_PORT="443"
-TYPESENSE_PROTOCOL="https"
-TYPESENSE_API_KEY=${TYPESENSE_API_KEY}
+#TYPESENSE_HOST="typesense.acdh-dev.oeaw.ac.at"
+#TYPESENSE_PORT="443"
+#TYPESENSE_PROTOCOL="https"
+#TYPESENSE_API_KEY=${TYPESENSE_API_KEY}
 ./build_app/shell/fetch_data.sh
 ./build_app/shell/script.sh
 ant -f build_app/ant/preprocess.xml
@@ -11,4 +11,4 @@ ant -f build_app/ant/preprocess.xml
 ./build_app/shell/denormalize.sh
 ant -f build_app/ant/build.xml
 ant -f build_app/ant/fixtures.xml
-python ./build_app/python/make_ts_index.py
+#python ./build_app/python/make_ts_index.py
