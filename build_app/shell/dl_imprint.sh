2b976319bd1bed6055c0f8afd0cea9fb66c7f2ad
fsanzl <>
2026-06-23 13:32:56 +0200

    locale

--- build_app/shell/dl_imprint.sh
+++ build_app/shell/dl_imprint.sh
@@ -12,3 +12,17 @@ echo '<div lang="en">' >> ${IMPRINT_XML}
 curl https://imprint.acdh.oeaw.ac.at/${REDMINE_ID}en >> ${IMPRINT_XML}
 echo "</div>" >> ${IMPRINT_XML}
 echo "</root>" >> ${IMPRINT_XML}
+
+
+REDMINE_ID="18716?format=xhtml&locale=en"
+IMPRINT_XML=./data/imprint_en.xml
+rm ${IMPRINT_XML}
+echo '<?xml version="1.0" encoding="UTF-8"?>'
+echo "<root>" >> ${IMPRINT_XML}
+echo '<div lang="de">' >> ${IMPRINT_XML}
+curl https://imprint.acdh.oeaw.ac.at/${REDMINE_ID}de >> ${IMPRINT_XML}
+echo "</div>"  >> ${IMPRINT_XML}
+echo '<div lang="en">' >> ${IMPRINT_XML}
+curl https://imprint.acdh.oeaw.ac.at/${REDMINE_ID}en >> ${IMPRINT_XML}
+echo "</div>" >> ${IMPRINT_XML}
+echo "</root>" >> ${IMPRINT_XML}
